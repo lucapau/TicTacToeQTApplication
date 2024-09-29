49501062538fdecdfc51fcc1654045f8656c9e56
lucapau <105868974+lucapau@users.noreply.github.com>
2024-09-29 13:11:26 +0100

    tic tac toe updated

---
+++ mainwindow.h
@@ -0,0 +1,46 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QLabel>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+struct Player {
+    QString name;
+    char marker;
+};
+
+struct Board {
+    char grid[3][3];
+};
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void onButtonClick(int row, int col);
+    void resetGame();
+
+private:
+    Ui::MainWindow *ui;
+    Board board;
+    Player player1, player2;
+    int currentPlayer;
+    QLabel *statusLabel;
+
+    void initializeGame();
+    void printBoard();
+    const char* checkWin();
+    void playerInput(int row, int col);
+    void updateBoardDisplay();
+};
+
+#endif // MAINWINDOW_H
